ef1c7556ccf7e18f92703025bc857b871555e130
rml6334 <rml6334@rit.edu>
2024-03-02 17:51:02 -0500

    Finished Timeslot and started appointment

---
+++ src/Appointment.java
@@ -0,0 +1,23 @@
+import java.sql.Time;
+
+public class Appointment extends Timeslot{
+    public Appointment(String name, Time startTime, Time endTime){
+        super(name, startTime, null, endTime);
+    }
+
+    @Override
+    public void setStartTime(Time startTime) {
+
+    }
+
+    @Override
+    public void setAmtTime(Time amtTime) {
+
+    }
+
+    @Override
+    public void setEndTime(Time endTime) {
+
+    }
+
+}
--- src/Timeslot.java
+++ src/Timeslot.java
@@ -5,4 +5,24 @@ public abstract class Timeslot {
     private Time startTime;
     private Time amtTime;
     private Time endTime;
+
+    protected Timeslot(String name, Time startTime, Time amtTime, Time endTime){
+        this.name = name;
+        this.startTime = startTime;
+        this.amtTime = amtTime;
+        this.endTime = endTime;
+    }
+    public abstract void setStartTime(Time startTime);
+    public Time getStartTime(){
+        return this.startTime;
+    }
+    public abstract void setAmtTime(Time amtTime);
+    public Time getAmtTime() {
+        return this.amtTime;
+    }
+
+    public abstract void setEndTime(Time endTime);
+    public Time getEndTime(){
+        return this.endTime;
+    }
 }
