4b669c0327c072ede6173d0326b758eaabcd5335
mblack <megcblack@gmail.com>
2024-03-02 17:53:58 -0500

    Task stubs

--- src/model/Task.java
+++ src/model/Task.java
@@ -1,8 +1,28 @@
 package model;
 
+import java.sql.Time;
+
 public class Task extends Timeslot {
     private PreferredTime prefTime;
     private Appointment before;
     private Appointment after;
 
+    protected Task(String name, Time startTime, Time amtTime, Time endTime) {
+        super(name, startTime, amtTime, endTime);
+    }
+
+    @Override
+    public void setStartTime(Time startTime) {
+
+    }
+
+    @Override
+    public void setAmtTime(Time amtTime) {
+
+    }
+
+    @Override
+    public void setEndTime(Time endTime) {
+
+    }
 }
