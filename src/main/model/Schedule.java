c304e614647479c8ddbcf45a136f3e0888aa63d4
mblack <megcblack@gmail.com>
2024-03-02 22:51:38 -0500

    TimeSchedule array in Schedule.java

--- src/main/model/Schedule.java
+++ src/main/model/Schedule.java
@@ -1,19 +1,20 @@
 package main.model;
 
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Schedule {
 
-    private static String[] times;
+    private static TimeSchedule[] times;
     private static ArrayList<Appointment> appointments;
 
 
     private static void setTimesArray () {
-        String[] times = new String[96];
+        TimeSchedule[] times = new TimeSchedule[96];
         for(int i = 0; i <= 23; i++) {
             for (int j = 0; j <= 45; j+=15) {
-                times[4*i + (j/15)] = i + ":" + j;
+                times[4*i + (j/15)] = new TimeSchedule(i + ":" + j);
             }
         }
         Schedule.times = times;
@@ -29,8 +30,8 @@ public class Schedule {
     }
     public static void main(String[] args) {
         setTimesArray();
-        for (String s: times) {
-
+        for (TimeSchedule s: times) {
+            System.out.println(s);
         }
 
     }
