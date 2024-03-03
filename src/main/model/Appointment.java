9995eddbb769b4a97e45cb8e8d52c8695b09fd83
rml6334 <rml6334@rit.edu>
2024-03-02 22:33:39 -0500

    Finished Timeslot and started appointment

--- src/main/model/Appointment.java
+++ src/main/model/Appointment.java
@@ -5,12 +5,6 @@ import java.sql.Time;
 public class Appointment extends Timeslot{
     public Appointment(String name, String startTime, String endTime){
         super(name, startTime, endTime);
-        int[] amtTime = TimeSchedule.timeDifference(super.getStartTime().getTime(), super.getEndTime().getTime());
-        super.setAmtTime(amtTime);
-    }
-
-    public void setTimeAmount(TimeSchedule starTime, TimeSchedule endTime){
-
     }
 
 }
