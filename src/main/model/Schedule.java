ca7205de520dfdacaf32f82ca4b4eea22e811314
mblack <megcblack@gmail.com>
2024-03-02 22:07:20 -0500

    Times array in Schedule

--- src/main/model/Schedule.java
+++ src/main/model/Schedule.java
@@ -1,4 +1,24 @@
 package main.model;
 
 public class Schedule {
+
+    private static String[] times;
+
+
+    private static void setTimesArray () {
+        String[] times = new String[96];
+        for(int i = 0; i <= 23; i++) {
+            for (int j = 0; j <= 45; j+=15) {
+                times[4*i + (j/15)] = i + ":" + j;
+            }
+        }
+        Schedule.times = times;
+    }
+
+    public static void main(String args[]) {
+        setTimesArray();
+        for (String s: times) {
+            System.out.println(s + " ");
+        }
+    }
 }
